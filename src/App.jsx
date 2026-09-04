18b1b536a440ce14c302dba9ed0a1c9a3487a636
victoroki <okiomeriv@gmail.com>
2026-09-04 16:22:29 +0300

    feat: add /create-account route for teacher sign-up

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import PrivacyPolicy from './pages/PrivacyPolicy.jsx'
 import TermsOfService from './pages/TermsOfService.jsx'
 import ContactUs from './pages/ContactUs.jsx'
 import GetStarted from './pages/GetStarted.jsx'
+import CreateAccount from './pages/CreateAccount.jsx'
 import Pods from './pages/Pods.jsx'
 import NotFound from './pages/NotFound.jsx'
 
@@ -33,6 +34,7 @@ export default function App() {
         <Route path="terms-of-service" element={<TermsOfService />} />
         <Route path="contact-us" element={<ContactUs />} />
         <Route path="get-started" element={<GetStarted />} />
+        <Route path="create-account" element={<CreateAccount />} />
         <Route path="pods" element={<Pods />} />
         <Route path="*" element={<NotFound />} />
       </Route>
